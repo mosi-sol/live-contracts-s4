d404f281f405e52930d573eebe02fef9efd660c9
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-11-09 20:48:51 +0330

    Create WordEncodeDecode.sol

---
+++ 17- Cipher Morse code 1/WordEncodeDecode.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+import "./CharToMorse.sol";
+// import "./MorseToChar.sol";
+
+// ["...","---","..."," ","...","---","..."]
+// ["s","o","s"," ","s","o","s"]
+contract TestWord is Cipher {
+    function concat(string memory _first, string memory _second) internal pure returns (string memory) {
+		return string(abi.encodePacked(_first, _second));
+	}
+
+    function morse(string[] memory c) public pure returns (string memory cipher) {
+        uint j = c.length;
+        string memory res;
+        string memory tmp;
+        for(uint i = 0; i < j; i++){
+            // tmp = morse_to_char(c[i]);
+            tmp = char_to_morse(c[i]);
+            res = concat(res, tmp);
+            tmp = "";
+        }
+        string memory fin = res;
+        res = "";
+        cipher = fin;
+    }
+}
