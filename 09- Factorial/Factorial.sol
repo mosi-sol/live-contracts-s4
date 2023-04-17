310c133e21e0fbc6f19c22b9846d01d37e5d17d0
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-04-17 05:16:51 +0330

    Update Factorial.sol

--- 09- Factorial/Factorial.sol
+++ 09- Factorial/Factorial.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8;
 // 5! = 5*4*3*2 = 120
 contract FactorialNumber {
     
+    // normal - look like the formula
     function factorNType1(uint num) public pure returns (uint) {
         uint cach = 1;
 
@@ -17,6 +18,7 @@ contract FactorialNumber {
         return cach;
     }
     
+    // advance - look like formula
     function factorNType2(uint num) public pure returns (uint) {
         uint cach = 1;
         
@@ -29,12 +31,27 @@ contract FactorialNumber {
         return cach;
     }
     
+    // recursion style - memory overflow in big numbers
     function factorNType3(uint num) public pure returns (uint) {
         if(num == 0 || num == 1) {
             return 1;
         }
 
         return num * factorNType3(num-1);
+    }
+    
+    // tail recursive - best style of coding code [functional programming]
+    function factorNType4(uint num) public pure returns (uint) {
+        return tailRecursive(num, 1);
+    }
+
+    function tailRecursive(uint num, uint result) private pure returns (uint) {
+        // result = 1; // this is require for start, at continue look like a mesh-tmp variable
+        if(num == 0 || num == 1) {
+            return result;
+        } else {
+            return tailRecursive(num - 1, result * num);
+        }
     }
     
 }
